179611aa10929f7c75266518058733946b4b01a4
meowWhat <1797793818@qq.com>
2020-04-19 17:49:09 +0800

    test

--- ask.js
+++ ask.js
@@ -1,10 +1,5 @@
 module.exports = [
   {
-    type: 'confirm',
-    name: 'private',
-    message: 'ths resgistery is private?',
-  },
-  {
     type: 'input',
     name: 'author',
     message: 'author?',
